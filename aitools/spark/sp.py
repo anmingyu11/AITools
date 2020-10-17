2720bd1a4a3935f6901f98adf0194fb2ded93d41
安明宇 <anmingyu@dhgate.com>
2020-10-17 21:40:11 +0800

    [spark]
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

---
+++ aitools/spark/sp.py
@@ -0,0 +1,8 @@
+def rename_columns(df, columns):
+    if not isinstance(columns, dict):
+        raise ValueError("'columns' should be a dict, like {'old_name_1':'new_name_1', 'old_name_2':'new_name_2'}")
+
+    for old_name, new_name in columns.items():
+        df = df.withColumnRenamed(old_name, new_name)
+
+    return df
