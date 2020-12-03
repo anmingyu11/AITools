c96e7741e0112c63fe1812edef32f14531828b76
安明宇 <anmingyu@dhgate.com>
2020-12-03 17:28:22 +0800

    [hdfs] package.
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- aitools/hdfs/hdfs.py
+++ aitools/hdfs/hdfs.py
@@ -18,3 +18,34 @@ def download_hdfs_csv(hdfs_path, local_path, prefix='part'):
                 local_path= local_csv_path,
                 hdfs_path= hdfs_path + item
             )
+
+class PackageHdfs():
+
+    def __init__(self):
+        self.fs = pyhdfs.HdfsClient('192.168.200.19:50070')
+
+    # 删除
+    def delFile(self,path):
+        fs = self.fs
+        fs.delete(path)
+
+    # 上传文件
+    def upload(self, fileName, tmpFile):
+        fs = self.fs
+        fs.copy_from_local(fileName, tmpFile)
+
+    # 新建目录
+    def mkdir(self, filePath):
+        fs = self.fs
+        if not fs.exists(filePath):
+            #os.system('hadoop fs -mkdir '+filePath)
+            fs.mkdirs(filePath)
+            return 'mkdir'
+        return 'exits'
+
+    # 重命名
+    def rename(self, srcPath, dstPath):
+        fs = self.fs
+        if not fs.exists(srcPath):
+            return
+        fs.rename(srcPath, dstPath)
