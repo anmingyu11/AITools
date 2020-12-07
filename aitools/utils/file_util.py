7c05325c16e1a588a21796c41d6b2b9894a6de16
安明宇 <anmingyu@dhgate.com>
2020-12-07 10:17:49 +0800

    [FileUtil] dump python object 2 pickle.
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- aitools/utils/file_util.py
+++ aitools/utils/file_util.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pathlib
 from pathlib import Path
 import shutil
@@ -52,6 +54,7 @@ def cp_dir(source, target):
         cp_file(f_source, f_target)
     pass
 
+
 def combine_csv(csv_files):
     path_list = csv_files
     dfs = []
@@ -62,8 +65,39 @@ def combine_csv(csv_files):
         dfs.append(df)
     pbar.close()
     dfs = pd.concat(dfs)
-    dfs = dfs.reset_index(drop = True)
+    dfs = dfs.reset_index(drop=True)
     return dfs
+
+
+def read_local_csv(local_path):
+    lists = os.listdir(local_path)
+    csvs = []
+    for item in lists:
+        if item.endswith('.csv'):
+            csvs.append(local_path + item)
+    df_res = combine_csv(csvs)
+    return df_res
+
+
+def rm_tree(path):
+    path = Path(path)
+    for child in path.glob('*'):
+        if child.is_file():
+            child.unlink()
+        else:
+            rm_tree(child)
+    path.rmdir()
+
 
 def remove(path):
     os.remove(path)
+
+
+def dump_to_pickle(obj, path):
+    with open(path, 'wb') as fo:
+        pickle.dump(obj, fo)
+
+
+def load_from_pickle(path):
+    with open(path,'rb') as fo:
+        return pickle.load(fo)
