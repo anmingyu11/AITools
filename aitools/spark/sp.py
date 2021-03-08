b5416ef2b44fce36e41ea996a6ce45707ef540b8
安明宇 <anmingyu@dhgate.com>
2021-03-08 11:11:48 +0800

    [spark] table exits.
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- aitools/spark/sp.py
+++ aitools/spark/sp.py
@@ -31,8 +31,29 @@ def get_partitions2(tb):
 def show_null(df):
     df.select(*(functions.sum(functions.col(c).isNull().cast("int")).alias(c) for c in df.columns)).show()
 
-def table_exists(tb):
-    return f.sql("SHOW TABLES LIKE '{}'".format(tb)).count() == 1
+def table_exists(table_name):
+    if '.' not in table_name:
+        raise Exception("must format like '*.*'")
+
+    database_list = f.sql('show databases').toPandas()['namespace'].values.tolist()
+    db_tb = {}
+
+    for e_db in database_list:
+        tables = f.sql("show tables in {}".format(e_db)).toPandas()['tableName'].values.tolist()
+        db_tb[e_db] = tables
+
+    splits = table_name.split('.')
+    if len(splits) != 2:
+        raise Exception('InCorrect formation {}'.format(table_name))
+
+    o_db_name = splits[0]
+    o_tb_name = splits[1]
+
+    if o_db_name not in db_tb:
+        display('db {} not exists'.format(o_db_name))
+        return False
+
+    return (o_db_name in db_tb) and (o_tb_name in db_tb[o_db_name])
 
 def split_vector(df,vec_col_name):
     df\
