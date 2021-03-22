4b21d6ce070155bb90fc9203c70f6a415780960f
安明宇 <anmingyu@dhgate.com>
2021-03-22 22:59:08 +0800

    Random Number Generator.
    Signed-off-by: 安明宇 <anmingyu@dhgate.com>

--- aitools/algorithm/random_number.c
+++ aitools/algorithm/random_number.c
@@ -1,6 +1,5 @@
 // https://en.wikipedia.org/wiki/Linear_congruential_generator
 int rand(seed){
-    if (sample > 0) {
     // 生成一个随机数
     // 64位整数
     next_random = next_random * (unsigned long long)25214903917 + 11;
@@ -11,5 +10,4 @@ int rand(seed){
     // 将这个数除以 65536 (2^16) 给我们一个 (0,1) 的分数。
     // 所以这段代码只是生成一个随机分数。
     if (ran < (next_random & 0xFFFF) / (real)65536) continue;
-    }
 }
